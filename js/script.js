93e947e308b29f9d49fe838cb49fea14c088b23e
MrBray20 <brianmone2@gmail.com>
2021-12-20 20:59:26 +0800

    login

--- js/script.js
+++ js/script.js
@@ -1,49 +1,5 @@
-function checkForm(){
-  const username = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-  const em = admin;
-  const pas = admin;
-  if(username==="admin"&&password==="admin"){
-    window.location.href="Katalog.html";
-  }
-  else{
-    
-    
-  }
-}
-
-
-
-function cekEmail(nilai, pesan) {
-  const email = /^([a-zA-Z0-9_.+-])+@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})+$/;
-  const em = nilai.value
-  if (nilai.value.match(email)) {
-    return true;
-  } else {
-    alert(pesan);
-    nilai.focus();
-    return false;
-  }
-}
-
-function Password(nilai, pesan) {
-  const alphaExp = /^[a-zA-Z]+$/;
-  if(nilai == ""){
-    alert("salah")
-    return false
-  }
-  else if (nilai.value.match(alphaExp)) {
-    return true;
-  } else {
-    alert(pesan);
-    nilai.focus();
-    return false;
-  }
-}
 
 function validasi() {
-  cekEmail(document.getElementById("email"), "Email tidak benar!!");
-  Number(document.getElementById("password"), "Telp. hanya ber isi Number!!");
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
   if(email == "" || password == ""){
@@ -53,7 +9,7 @@ function validasi() {
     window.location.href="Katalog.html";
   }
   else{
-    document.getElementById('kosong').innerHTML = salah;
+    alert("email atau password yang anda masukkan salah");
   }
   
 }
