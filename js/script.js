c1d3949d2f80865e74a78eae50bb075d49a7ed09
MrBray20 <brianmone2@gmail.com>
2021-12-20 19:23:46 +0800

    login

--- js/script.js
+++ js/script.js
@@ -1,24 +1,60 @@
-const searchInputSidenav = document.getElementById('search-input-sidenav');
-const sidenavOptions = document.querySelectorAll('#sidenav-3 li .sidenav-link');
 
-searchInputSidenav.addEventListener('input', () => {
-  const filter = searchInputSidenav.value.toLowerCase();
-  showSidenavOptions();
-  const valueExist = !!filter.length;
-
-  if (valueExist) {
-    sidenavOptions.forEach((el) => {
-      const elText = el.textContent.trim().toLowerCase();
-      const isIncluded = elText.includes(filter);
-      if (!isIncluded) {
-        el.style.display = 'none';
-      }
-    });
+function checkForm(){
+  const username = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const em = admin;
+  const pas = admin;
+  if(username==="admin"&&password==="admin"){
+    window.location.href="Katalog.html";
   }
-});
+  else{
+    
+    
+  }
+}
 
-const showSidenavOptions = () => {
-  sidenavOptions.forEach((el) => {
-    el.style.display = 'flex';
-  });
-};
+
+
+function cekEmail(nilai, pesan) {
+  const email = /^([a-zA-Z0-9_.+-])+@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})+$/;
+  const em = nilai.value
+  if (nilai.value.match(email)) {
+    return true;
+  } else {
+    alert(pesan);
+    nilai.focus();
+    return false;
+  }
+}
+
+function Password(nilai, pesan) {
+  const alphaExp = /^[a-zA-Z]+$/;
+  if(nilai == ""){
+    alert("salah")
+    return false
+  }
+  else if (nilai.value.match(alphaExp)) {
+    return true;
+  } else {
+    alert(pesan);
+    nilai.focus();
+    return false;
+  }
+}
+
+function validasi() {
+  cekEmail(document.getElementById("email"), "Email tidak benar!!");
+  Number(document.getElementById("password"), "Telp. hanya ber isi Number!!");
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  if(email == "" || password == ""){
+    alert("tidak boleh kosong");
+  }
+  else if(email == "JohnStore@bookstore.com" && password == "johnstore"){
+    window.location.href="Katalog.html";
+  }
+  else{
+    document.getElementById('kosong').innerHTML = salah;
+  }
+  
+}
